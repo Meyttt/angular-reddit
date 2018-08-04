da0d4b234cd8e859f85b94de4dc59dd43198d97f
Meyttt <sabermeyt@gmail.com>
2018-08-04 10:48:37 +0300

    Поддеплой

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  articles: Article[];
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  constructor() {
+    this.articles = [new Article('Java 10', 'https://stackoverflow.org', 42),
+    new Article('Angular 6', 'http://angular.io')];
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) =>  b.votes - a.votes);
+  }
+
 }
